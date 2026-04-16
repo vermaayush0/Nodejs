866461f307e35affc71219a72f6b6fe1e34e1a2f
Ayush Verma <142798351+vermaayush0@users.noreply.github.com>
2026-04-16 14:01:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const mongoose = require('mongoose');
-const User=require('./model/user')
+const User = require('./model/user');
+const bcrypt = require('bcrypt'); // for password hashing
+
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/authDB')
-  .then(() => {console.log('Connected to MongoDB')})
-  .catch(()=> {console.error('Could not connect to MongoDB')});
-
-
+  .then(() => { console.log('Connected to MongoDB'); })
+  .catch(() => { console.error('Could not connect to MongoDB'); });
 
 app.set('view engine', 'ejs');
 
@@ -19,15 +20,29 @@ app.get('/', (req, res) => {
 app.get('/signup', (req, res) => {
   res.render("signup");
 });
+app.post('/signup', async (req, res) => {
+  const { username, password, email } = req.body;
+
+  let existingUser = await User.findOne({ username });
+  if (existingUser) {
+    return res.redirect('/signup'); // Redirect back to signup if username already exists
+  }
+
+  const saltRounds = 10; // Number of rounds for salting
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  await User.create({ username, password: hashedPassword, email });
+
+  res.redirect('/login');
+});
 
 app.get('/login', (req, res) => {
-  res.render("login");
+  res.render('login');
 });
 
 app.get('/payment', (req, res) => {
-  res.render("payment");
+  res.render('payment');
 });
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
 });
